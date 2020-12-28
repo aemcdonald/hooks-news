20a5182fbc61c4660bdf1951a4f492bf0b3d3a25
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-28 13:54:39 -0700

    Create fetch request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 export default function App() {
+  useEffect(() => {
+    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => {
+        console.log(response.data.hits);
+      }, [])
+  })
+  
   return(
     <div>App</div>
   )
