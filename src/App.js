6d9c901f276bcfc04e5238ea6e0406f87ca8fafb
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-28 13:56:04 -0700

    Set fetched data in state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function App() {
+  const [results, setResults] = useState([])
   useEffect(() => {
     axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
       .then(response => {
-        console.log(response.data.hits);
+        // console.log(response.data.hits);
+        setResults(response.data.hits)
       }, [])
   })
   
