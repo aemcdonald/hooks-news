81615dde3017d814d07fd49bd1a29234f03eacfa
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-28 15:10:59 -0700

    Refactor using async await

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import axios from 'axios';
 export default function App() {
   const [results, setResults] = useState([])
   useEffect(() => {
-    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-      .then(response => {
-        // console.log(response.data.hits);
-        setResults(response.data.hits)
-      }, [])
-  })
+    getResults()
+  }, [])
+
+  const getResults = async () => {
+    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+    setResults(response.data.hits)
+  }
   
   return(
     <>
